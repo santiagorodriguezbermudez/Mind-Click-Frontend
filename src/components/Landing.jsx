2f406efe0ffb761e975d2d24d8e73ef4c89f79c5
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-11-06 16:24:14 -0500

    Added landing page context component

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
+import ReactRotatingText from 'react-rotating-text';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
-const Landing = () => (
-  <div>
-    This is where home goes
-  </div>
-);
+const Landing = () => {
+  const arrayText = ['a Psiquiatris', 'your coach', 'therapist', 'psychologist'];
+
+  return (
+    <div>
+      <main>
+        <h1 className="main-title">
+          Find your
+          {' '}
+          <ReactRotatingText items={arrayText} />
+          .
+        </h1>
+      </main>
+      <div>
+        Value Proposition
+      </div>
+      <section>
+        <Carousel
+          autoPlay="true"
+        >
+          <div>
+            <p>First Testimonial</p>
+          </div>
+          <div>
+            <p>Second Testimonial</p>
+          </div>
+          <div>
+            <p>Third Testimonial</p>
+          </div>
+        </Carousel>
+      </section>
+      <div>
+        Call to Action
+      </div>
+    </div>
+  );
+};
 
 export default Landing;
