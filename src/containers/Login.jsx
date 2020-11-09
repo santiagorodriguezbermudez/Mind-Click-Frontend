51df7e28934c9b893a76ba0056bf7d607b004582
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-11-09 12:58:25 -0500

    Added styles to login

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -5,6 +5,7 @@ import { Redirect, Link } from 'react-router-dom';
 import Loading from '../components/Loading';
 import { loginApiCall } from '../actions/api';
 import { validateCurrentToken } from '../helpers/tokenLocalStorage';
+import '../assets/styles/login.css';
 
 const Login = ({ application, loginApiCall, authenticationMessage }) => {
   const [email, setEmail] = React.useState('');
@@ -20,32 +21,37 @@ const Login = ({ application, loginApiCall, authenticationMessage }) => {
   };
 
   const renderForm = () => (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor={email}>
-        Email
-        <input
-          type="email"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-          required
-          placeholder="Type in your email address"
-        />
-      </label>
-      <label htmlFor={password}>
-        Password
-        <input
-          type="password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-          required
-          placeholder="Create your password"
-        />
-      </label>
-      <input type="submit" value="Login" />
-      <span>
-        <Link to="/signup">Create an Account</Link>
-      </span>
-    </form>
+    <div className="login">
+      <form onSubmit={handleSubmit} className="form">
+        <h1>Log In</h1>
+        <label htmlFor={email}>
+          Email
+          <input
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            required
+            placeholder="Type in your email address"
+            className="form-input"
+          />
+        </label>
+        <label htmlFor={password}>
+          Password
+          <input
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            required
+            placeholder="Create your password"
+            className="form-input"
+          />
+        </label>
+        <input type="submit" value="Login" className="submit-form" />
+        <span>
+          <Link to="/signup">Create an Account</Link>
+        </span>
+      </form>
+    </div>
   );
 
   if (validateCurrentToken()) {
